6eabe3e9f1a367824c5b4e30591d89f165624cfe
MarcusKyung <Kyungmj@gmail.com>
2023-04-18 20:35:40 -0700

    add updates to giphy-service.js

--- src/giphy-service.js
+++ src/giphy-service.js
@@ -1,18 +1,18 @@
 export default class GiphyService{
   static getGif(gifSearch, gifLimit, gifRating) {
     return new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${gifSearch}&limit=${gifLimit}&offset=0&rating=${gifRating}&lang=en`;
-    request.addEventListener("loadend", function() {
-      const response = JSON.parse(this.responseText);
-      if (this.status === 200) {
-        resolve([response, gifLimit]);
-      } else {
-        reject([this, response, gifSearch]); 
-      }
-    });
-    request.open("GET", url, true);
-    request.send();
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${gifSearch}&limit=${gifLimit}&offset=0&rating=${gifRating}&lang=en`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, gifLimit]);
+        } else {
+          reject([this, response, gifSearch]); 
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
     });
   }
 }
